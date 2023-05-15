93a2924a4caa13cb9c38d9ed913d3c42449e7ee4
connor-kfitz <connorkfitzsimmons@gmail.com>
2023-05-15 19:33:38 -0400

    Created Base File Structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './views/home';
+
+import './styles/main.scss';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main class="main">
+        <Home/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
